d699fb76799e3cc6bf912e0edcc1e72d3c05dc15
Mr-Rafael <rafael.mazariegosr@gmail.com>
2025-05-13 21:45:59 -0600

    added conversion from text node to html leaf node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -15,15 +15,55 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node, node2)
     
     def test_not_eq(self):
-        node = TextNode("This is a link", TextType.NORMAL)
+        node = TextNode("This is a link", TextType.TEXT)
         node2 = TextNode("This is a link", TextType.BOLD)
         self.assertNotEqual(node, node2)
     
     def test_not_eq_url(self):
-        node = TextNode("This is a link", TextType.NORMAL, "URL")
-        node2 = TextNode("This is a link", TextType.NORMAL)
+        node = TextNode("This is a link", TextType.TEXT, "URL")
+        node2 = TextNode("This is a link", TextType.TEXT)
         self.assertNotEqual(node, node2)
 
+    def test_text_type(self):
+        node = TextNode("This is a simple text node", TextType.TEXT)
+        html_node = TextNode.textnode_to_html_node(node).to_html()
+        expected_node = "This is a simple text node"
+        self.assertEqual(html_node, expected_node)
+
+    def test_bold_type(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = TextNode.textnode_to_html_node(node).to_html()
+        expected_node = "<b>This is a bold node</b>"
+        self.assertEqual(html_node, expected_node)
+
+    def test_italic_type(self):
+        node = TextNode("This is an italic node", TextType.ITALIC)
+        html_node = TextNode.textnode_to_html_node(node).to_html()
+        expected_node = "<i>This is an italic node</i>"
+        self.assertEqual(html_node, expected_node)
+
+    def test_code_type(self):
+        node = TextNode("This is a\ncode node\nbeep boop", TextType.CODE)
+        html_node = TextNode.textnode_to_html_node(node).to_html()
+        expected_node = "<code>This is a\ncode node\nbeep boop</code>"
+        self.assertEqual(html_node, expected_node)
+    
+    def test_link_type(self):
+        node = TextNode("click me", TextType.LINK, "link.com")
+        html_node = TextNode.textnode_to_html_node(node).to_html()
+        expected_node = '<a href="link.com">click me</a>'
+        self.assertEqual(html_node, expected_node)
+    
+    def test_image_type(self):
+        node = TextNode("image description", TextType.IMAGE, "pic.png")
+        html_node = TextNode.textnode_to_html_node(node).to_html()
+        expected_node = '<img src="pic.png" alt="image description"></img>'
+        self.assertEqual(html_node, expected_node)
+
+    def test_invalid_type(self):
+        with self.assertRaises(ValueError):
+            node = TextNode("invalid node", "INVALID")
+            TextNode.textnode_to_html_node(node)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -20,3 +21,22 @@ class TextNode():
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+    def textnode_to_html_node(text_node):
+        match text_node.text_type:
+            case TextType.TEXT:
+                return LeafNode(value=text_node.text)
+            case TextType.BOLD:
+                return LeafNode(tag="b", value=text_node.text)
+            case TextType.ITALIC:
+                return LeafNode(tag="i", value=text_node.text)
+            case TextType.CODE:
+                return LeafNode(tag="code", value=text_node.text)
+            case TextType.LINK:
+                link_prop = {"href": text_node.url}
+                return LeafNode(tag="a", value=text_node.text, props=link_prop)
+            case TextType.IMAGE:
+                image_props = {"src": text_node.url, "alt": text_node.text}
+                return LeafNode(tag="img", value="", props=image_props)
+            case _:
+                raise ValueError(f"Failed to convert a textnode of type {text_node.text_type} to html")
