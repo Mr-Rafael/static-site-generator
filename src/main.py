ceea26faeb08fcd4b3af13288a6fde38e5979de1
Mr-Rafael <rafael.mazariegosr@gmail.com>
2025-05-20 20:55:46 -0600

    added function to refresh the contents of /public with the contents on /static

--- src/main.py
+++ src/main.py
@@ -1,9 +1,32 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 
 def main():
-    example = TextNode("1", TextType.LINK, "3")
-    print(f"first node: {example}")
-    without_url = TextNode("1", TextType.NORMAL)
-    print(f"scond node: {without_url}")
+    copy_source_contents()
+
+def copy_source_contents():
+    source_directory = "static"
+    destination_directory = "public"
+    clear_destination_directory(destination_directory)
+    copy_contents(source_directory, destination_directory)
+
+def clear_destination_directory(directory):
+    if os.path.isdir(directory):
+        shutil.rmtree(directory)
+        os.mkdir(directory, mode=0o777)
+    else:
+        os.mkdir(destination_directory, mode=0o777)
+
+def copy_contents(source_directory, destination_directory):
+    paths_list = os.listdir(source_directory)
+    for path in paths_list:
+        source_path = os.path.join(source_directory, path)
+        destination_path = os.path.join(destination_directory, path)
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, destination_path)
+        elif os.path.isdir(source_path):
+            os.mkdir(destination_path)
+            copy_contents(source_path, destination_path)
 
 main()
