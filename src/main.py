f29025d4020621d761a577ef5ab57894d58bff4a
Mr-Rafael <rafael.mazariegosr@gmail.com>
2025-05-21 22:52:55 -0600

    prepared the site for depoyment to github pages

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
 import os
 import shutil
+import sys
 from textnode import TextNode, TextType
 from markdown_parser import *
 
 def main():
     copy_source_contents()
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = get_base_path
+    generate_pages_recursive("content", "template.html", "docs", basepath)
+
+def get_base_path():
+    if len(sys.argv) > 0:
+        return sys.argv[0]
+    return "/"
 
 def copy_source_contents():
     source_directory = "static"
-    destination_directory = "public"
+    destination_directory = "docs"
     clear_destination_directory(destination_directory)
     copy_contents(source_directory, destination_directory)
 
@@ -31,7 +38,7 @@ def copy_contents(source_directory, destination_directory):
             os.mkdir(destination_path)
             copy_contents(source_path, destination_path)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with(open(from_path, 'r') as markdown_file):
         markdown_content = markdown_file.read()
@@ -41,15 +48,17 @@ def generate_page(from_path, template_path, dest_path):
     title_string = extract_title(markdown_content)
     template_content = template_content.replace(f"{{{{ Title }}}}", title_string)
     template_content = template_content.replace(f"{{{{ Content }}}}", html_string)
+    template_content = template_content.replace("href=\"/", f"href\"={base_path}")
+    template_content = template_content.replace("src=\"/", f"src\"={base_path}")
     create_directory_if_nonexistent(dest_path)
     with(open(dest_path, 'w') as html_file):
         html_file.write(template_content)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, base_path):
     found_html_paths = find_html_files(dir_path_content)
     for html_file_path in found_html_paths:
         destination_file_path = html_file_path.replace(dir_path_content, dest_dir_path).replace(".md", ".html")
-        generate_page(html_file_path, template_path, destination_file_path)
+        generate_page(html_file_path, template_path, destination_file_path, base_path)
 
 def find_html_files(start_directory):
     paths_list = os.listdir(start_directory)
